0b3e23c6dde8c961da89fe7a9432ae8aa5836f67
thani <thani@Scooby.home>
2022-08-30 12:18:20 -0500

    New Project1

---
+++ src/Training1/Practice.java
@@ -0,0 +1,20 @@
+package Training1;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi Welcome to Devops Training");
+		System.out.println(120+100);
+
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
